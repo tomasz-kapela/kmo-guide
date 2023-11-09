c5cdbdedcb3061f246a048ec6e93ed0ea16ba3bc
Tomasz Kapela <92725383+tomasz-kapela@users.noreply.github.com>
2023-11-09 11:04:46 +0100

    Update send_task.sh

--- utils/send_task.sh
+++ utils/send_task.sh
@@ -5,13 +5,17 @@ USAGE:
 Sends script with given parameters to slurm queque 
 EOH
 
-echo "Sending task: ${@:2}"
-echo "to queque: $1"
-
 if [ $# -lt 2 ]; then
 	echo $HELP
         exit 0
 fi
+if [ "$1" != "kmo" ] && [ "$1" != "cpu" ]; then
+	echo "Slurm queque name has to be kmo or cpu."
+	exit 1
+fi	
+
+echo "Sending task: ${@:2}"
+echo "to queque: $1"
 
 sbatch -p $1 <<EOT
 #!/bin/bash
